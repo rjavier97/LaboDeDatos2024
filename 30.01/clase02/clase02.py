44a8f8a1c3e791850cf01db134db389d88d92fcb
rjavier97 <rjavier.suarez@hotmail.com>
2024-02-05 18:23:48 -0300

    update

--- 30.01/clase02/clase02.py
+++ 30.01/clase02/clase02.py
@@ -5,8 +5,11 @@
 # Este es un archivo temporal.
 # """
 
-# """f9 para correr linea por linea,
-# shift + enter para correr linea indicada"""
+# =============================================================================
+# f9 para correr linea por linea,
+# shift + enter para correr linea indicada
+# =============================================================================
+
 
 # """Ejercicio"""
 # """
@@ -29,63 +32,44 @@
 
 # f = open('datame.txt', 'rt', encoding="utf-8")
 # data = f.read()
-# print(data)
+# print("Mientras esta abierto el archivo\n"+data)
 # f.close()
+# print("Luego de cerrar archivo\n"+data)
 
 # with open('datame.txt', 'rt', encoding="utf-8") as file: # otra forma de abrir archivos
-#  data = file.read()
-#  # 'data' es una cadena con todo el texto en el archivo
+#   data = file.read()
+#   # 'data' es una cadena con todo el texto en el archivo
 # data
 # print(data)
 
 # nombre_archivo = 'cronograma_sugerido.csv'
 # with open(nombre_archivo, 'rt', encoding="utf-8") as file:
 #     for line in file:
+#         print(line)
 #         datos_linea = line.split(',')
 #         print(datos_linea)
 #         print(datos_linea[1])
 
 
-import csv 
-nombre_archivo = 'cronograma_sugerido.csv'
-# with open(nombre_archivo, 'rt', encoding="utf-8") as file :
-def registros(nombre_archivo):
-            lista = []
-            with open(nombre_archivo, 'rt', encoding="utf-8") as f :
-                filas = csv.reader(f)
-                # print(filas)
-                encabezado = next(filas)
-                # print(encabezado)
-                for fila in filas :
-                    registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
-                    # print(registro)
-                    lista.append(registro)                  # lo agrego a la lista
-                    # print(lista)
-            return lista 
-print(registros(nombre_archivo)) 
-
-# print(registros('cronograma_sugerido.csv'))
-#     lista = []
-#     for line in file: 
-#         datos_linea = line.split(',')
-#         print(datos_linea[1])
-#         lista.append(datos_linea[1])
-#     print(lista)
-# """    
-
-# """
 # import csv 
+# nombre_archivo = 'cronograma_sugerido.csv'
+# # registros devuelve un diccionario con el encabezado como claves, 
+# # y los valores las entradas de cada fila
 # def registros(nombre_archivo):
 #     lista = []
-#     with open(nombre_archivo, 'rt') as f :
+#     with open(nombre_archivo, 'rt', encoding="utf-8") as f :
 #         filas = csv.reader(f)
+#         # print(filas) #Muestra: <_csv.reader object at 0x0000022E1175B280>
 #         encabezado = next(filas)
+#         print("Este es el encabezado\n"+str(encabezado)+'\n')
 #         for fila in filas :
 #             registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
-#             lista.append(registro)                  # lo agrego a la lista
+#             # print(registro)
+#             lista.append(registro)   # lo agrego a la lista
+#             # print(lista)
 #     return lista 
-# print(registros('cronograma_sugerido.csv'))
-# """
+# print(registros(nombre_archivo)) 
+
 
 # """ Ejercicios ---------------------------------------------- """
 # """
@@ -99,55 +83,63 @@ print(registros(nombre_archivo))
 # print(generala_tirar())
 # """
 
-# """
+
+# #Imprime las lineas que incluye"estudiantes"
 # def contieneEstudiante(nombre_archivo):
-#     with open(nombre_archivo, 'rt') as file :
+#     with open(nombre_archivo, 'rt', encoding="utf-8") as file :
 #         lista_lineas = file.readlines() 
+#         # print("Lista de lineas del archivo:\n"+str(lista_lineas))
 #         for linea in lista_lineas :
 #             if "estudiante" in linea :
 #                 print(linea)
-# contieneEstudiante('datame.txt')
-# """
+# contieneEstudiante('datame.txt') 
 
-# """
+
+
 # def listadoMaterias(nombre_archivo): 
-#     with open(nombre_archivo, 'rt') as file : 
+#     with open(nombre_archivo, 'rt', encoding="utf-8") as file : 
 #         lista_materias = []
+#         next(file)  #Aca me salteo el encabezado(primerafila)
 #         for line in file: 
-#             datos_linea = line.split(',')
+#             datos_linea = line.split(',') #Hago de c/linea una lista
 #             print(datos_linea)
 #           # print(datos_linea[1])
 #             lista_materias.append(datos_linea[1])
-#         return lista_materias
-# print(listadoMaterias('cronograma_sugerido.csv'))
-# """
+#     return lista_materias
+# listadoMaterias('cronograma_sugerido.csv')
 
-# """
+
 # def cuantas_materias(n):
 #     cantidad_materias = 0 
-#     with open('cronograma_sugerido.csv', 'rt') as file :
+#     with open('cronograma_sugerido.csv', 'rt', encoding="utf-8") as file :
 #         for line in file :
 #             datos_linea = line.split(',')
 #             if str(n) in datos_linea :
 #                 cantidad_materias += 1
-#         return cantidad_materias
-# print(cuantas_materias(6)) 
-# """
+#     return cantidad_materias
+# print("En el cuatrimestre deberá cursar",cuantas_materias(8),"materias")
 
-# import csv
-# def materias_cuatrimestre(nombre_archivo, n):
-#     lista = []
-#     lista_diccionarios = []
-#     with open(nombre_archivo, 'rt') as f :
-#         filas = csv.reader(f)
-#         encabezado = next(filas)
-#         for fila in filas :
-#             registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
-#             lista.append(registro)                  # lo agrego a la lista
-#         for i in lista :
-#             if i['Cuatrimestre'] == str(n) :
-#                 lista_diccionarios.append(i) 
-#         return lista_diccionarios
+
+
+# =============================================================================
+# Funcion que devuelve una lista de diccionarios con la información de 
+# las materias sugeridas para cursar el n-ésimo cuatrimestre.
+# =============================================================================
+import csv
+def materias_cuatrimestre(nombre_archivo, n):
+    lista = []
+    lista_diccionarios = []
+    with open(nombre_archivo, 'rt', encoding="utf-8") as f :
+        filas = csv.reader(f)
+        encabezado = next(filas)
+        for fila in filas :
+            registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
+            lista.append(registro)                  # lo agrego a la lista
+        for i in lista :
+            if i['Cuatrimestre'] == str(n) :
+                lista_diccionarios.append(i) 
+    return lista_diccionarios
+materias_cuatrimestre("cronograma_sugerido.csv", 5)
                 
     
             
---
+++ 30.01/clase02/guia2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 13:34:46 2024
+
+@author: pc
+"""
+import random, math, csv
+import numpy as np
+import pandas as pd
+
+
