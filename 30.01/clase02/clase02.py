ab7b786e03884f11baeb1aefc0a6c1e1b7e3ab18
rjavier97 <rjavier.suarez@hotmail.com>
2024-02-06 01:05:14 -0300

    guia2 terminada

--- 30.01/clase02/clase02.py
+++ 30.01/clase02/clase02.py
@@ -13,19 +13,19 @@
 
 # """Ejercicio"""
 # """
-# def traductor_geringoso(lista):
-#     vocales =  ["a", "e", "i", "o", "u"]
-#     diccionario = {}
-#     palabra=""
-#     for i in lista :
-#         for j in i :
-#             if j in vocales : 
-#                 palabra = palabra + j + "p"+ j
-#             else :
-#                 palabra = palabra + j
-#         diccionario[i] = palabra
-#         palabra = ""
-#     return diccionario     
+def traductor_geringoso(lista):
+    vocales =  ["a", "e", "i", "o", "u"]
+    diccionario = {}
+    palabra=""
+    for i in lista :
+        for j in i :
+            if j in vocales : 
+                palabra = palabra + j + "p"+ j
+            else :
+                palabra = palabra + j
+        diccionario[i] = palabra
+        palabra = ""
+    return diccionario     
 # """
 
 # """print(traductor_geringoso(["perro", "gato"])) """
@@ -55,68 +55,68 @@
 # nombre_archivo = 'cronograma_sugerido.csv'
 # # registros devuelve un diccionario con el encabezado como claves, 
 # # y los valores las entradas de cada fila
-# def registros(nombre_archivo):
-#     lista = []
-#     with open(nombre_archivo, 'rt', encoding="utf-8") as f :
-#         filas = csv.reader(f)
-#         # print(filas) #Muestra: <_csv.reader object at 0x0000022E1175B280>
-#         encabezado = next(filas)
-#         print("Este es el encabezado\n"+str(encabezado)+'\n')
-#         for fila in filas :
-#             registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
-#             # print(registro)
-#             lista.append(registro)   # lo agrego a la lista
-#             # print(lista)
-#     return lista 
+def registros(nombre_archivo):
+    lista = []
+    with open(nombre_archivo, 'rt', encoding="utf-8") as f :
+        filas = csv.reader(f)
+        # print(filas) #Muestra: <_csv.reader object at 0x0000022E1175B280>
+        encabezado = next(filas)
+        print("Este es el encabezado\n"+str(encabezado)+'\n')
+        for fila in filas :
+            registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
+            # print(registro)
+            lista.append(registro)   # lo agrego a la lista
+            # print(lista)
+    return lista 
 # print(registros(nombre_archivo)) 
 
 
 # """ Ejercicios ---------------------------------------------- """
 # """
-# import random 
-# def generala_tirar():
-#     lista = [1,2,3,4,5]
-#     lista_aleatoria=[]
-#     for i in range(1,6):
-#         lista_aleatoria.append(random.choice(lista))
-#     return lista_aleatoria
+import random 
+def generala_tirar():
+    lista = [1,2,3,4,5]
+    lista_aleatoria=[]
+    for i in range(1,6):
+        lista_aleatoria.append(random.choice(lista))
+    return lista_aleatoria
 # print(generala_tirar())
 # """
 
 
-# #Imprime las lineas que incluye"estudiantes"
-# def contieneEstudiante(nombre_archivo):
-#     with open(nombre_archivo, 'rt', encoding="utf-8") as file :
-#         lista_lineas = file.readlines() 
-#         # print("Lista de lineas del archivo:\n"+str(lista_lineas))
-#         for linea in lista_lineas :
-#             if "estudiante" in linea :
-#                 print(linea)
+#Imprime las lineas que incluye"estudiantes"
+def contieneEstudiante(nombre_archivo):
+    with open(nombre_archivo, 'rt', encoding="utf-8") as file :
+        lista_lineas = file.readlines() 
+        # print("Lista de lineas del archivo:\n"+str(lista_lineas))
+        for linea in lista_lineas :
+            if "estudiante" in linea :
+                print(linea)
 # contieneEstudiante('datame.txt') 
 
 
 
-# def listadoMaterias(nombre_archivo): 
-#     with open(nombre_archivo, 'rt', encoding="utf-8") as file : 
-#         lista_materias = []
-#         next(file)  #Aca me salteo el encabezado(primerafila)
-#         for line in file: 
-#             datos_linea = line.split(',') #Hago de c/linea una lista
-#             print(datos_linea)
-#           # print(datos_linea[1])
-#             lista_materias.append(datos_linea[1])
-#     return lista_materias
+def listadoMaterias(nombre_archivo): 
+    with open(nombre_archivo, 'rt', encoding="utf-8") as file : 
+        lista_materias = []
+        next(file)  #Aca me salteo el encabezado(primerafila)
+        for line in file: 
+            datos_linea = line.split(',') #Hago de c/linea una lista
+            print(datos_linea)
+          # print(datos_linea[1])
+            lista_materias.append(datos_linea[1])
+    return lista_materias
 # listadoMaterias('cronograma_sugerido.csv')
 
 
-# def cuantas_materias(n):
-#     cantidad_materias = 0 
-#     with open('cronograma_sugerido.csv', 'rt', encoding="utf-8") as file :
-#         for line in file :
-#             datos_linea = line.split(',')
-#             if str(n) in datos_linea :
-#                 cantidad_materias += 1
-#     return cantidad_materias
+def cuantas_materias(n):
+    cantidad_materias = 0 
+    with open('cronograma_sugerido.csv', 'rt', encoding="utf-8") as file :
+        for line in file :
+            datos_linea = line.split(',')
+            if str(n) in datos_linea :
+                cantidad_materias += 1
+    return cantidad_materias
 # print("En el cuatrimestre deberá cursar",cuantas_materias(8),"materias")
 
 
@@ -133,13 +133,14 @@ def materias_cuatrimestre(nombre_archivo, n):
         filas = csv.reader(f)
         encabezado = next(filas)
         for fila in filas :
+            print(fila)
             registro = dict(zip(encabezado, fila))  # armo el diccionario de cada fila
             lista.append(registro)                  # lo agrego a la lista
         for i in lista :
             if i['Cuatrimestre'] == str(n) :
                 lista_diccionarios.append(i) 
     return lista_diccionarios
-materias_cuatrimestre("cronograma_sugerido.csv", 5)
+# materias_cuatrimestre("cronograma_sugerido.csv", 5)
                 
     
             
--- 30.01/clase02/guia2.py
+++ 30.01/clase02/guia2.py
@@ -4,8 +4,133 @@ Created on Mon Feb  5 13:34:46 2024
 
 @author: pc
 """
-import random, math, csv
+import random
+import math
+import csv
 import numpy as np
 import pandas as pd
 
 
+# # Ejercicio1--------------------------------------
+
+def leer_parque(nombre_archivo, parque)->list:
+    lista_diccionarios = []
+    with open(nombre_archivo, 'rt', encoding="utf-8") as f:
+        filas = csv.reader(f)
+        encabezado = next(filas)
+        for fila in filas :
+            if parque in fila:
+                lista_diccionarios.append(dict(zip(encabezado, fila)) )
+    return lista_diccionarios 
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")  
+# len(leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")) #Da 690 
+
+
+# # Ejercicio2--------------------------------------
+# Devuelve el conjunto de especies(columna: 'nombre_com') de c/arbol
+def especies(lista_arboles)->set:
+    conjunto_especies = set() # es un conjunto de elementos unicos
+    for arbol in lista_arboles:
+        conjunto_especies.add(arbol["nombre_com"])
+    return conjunto_especies
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+# especies(a)
+# a es lista_arboles
+
+
+# # Ejercicio3--------------------------------------
+def contar_ejemplares(lista_arboles)->dict:
+    conjunto_especies = especies(lista_arboles) # es un conjunto de elementos unicos
+    listaValores = [0]*len(conjunto_especies)
+    diccionario_ejemplares = dict(zip(conjunto_especies,listaValores.copy()))   
+    for arbol in lista_arboles:
+        diccionario_ejemplares[arbol["nombre_com"]]+=1
+    return diccionario_ejemplares
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+# contar_ejemplares(a)
+
+
+# # Ejercicio4--------------------------------------
+def obtener_alturas(lista_arboles, especie)->list:
+    alturas_especie = []
+    for arbol in lista_arboles :
+        if arbol["nombre_com"] == especie :
+            alturas_especie.append(float(arbol['altura_tot']))
+    return alturas_especie 
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+# obtener_alturas(a, "Jacarandá")
+
+def altura_Max_y_Promedio():
+    a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+    alturasA = obtener_alturas(a, "Jacarandá")
+    b = leer_parque("arbolado-en-espacios-verdes.csv", "ANDES, LOS")
+    alturasB = obtener_alturas(b, "Jacarandá")
+    c = leer_parque("arbolado-en-espacios-verdes.csv", "CENTENARIO")
+    alturasC = obtener_alturas(c, "Jacarandá")
+    print("Medida General Paz Los Andes Centenario")
+    print("max", max(alturasA) ,max(alturasB), max(alturasC) )
+    print("prom", round(sum(alturasA)/len(alturasA),2), round(sum(alturasB)/len(alturasB),2), round(sum(alturasC)/len(alturasC),2) )
+# altura_Max_y_Promedio()
+
+
+# # Ejercicio5--------------------------------------
+def obtener_inclinaciones(lista_arboles, especie)->list:
+    inclinaciones_especie = []
+    for arbol in lista_arboles :
+        if arbol["nombre_com"] == especie :
+            inclinaciones_especie.append(int(arbol['inclinacio']))
+    return inclinaciones_especie
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ")
+# obtener_inclinaciones(a, "Jacarandá")
+
+
+# # Ejercici6--------------------------------------
+def especimen_mas_inclinado(lista_arboles):
+    conjunto_especies_lista = list(especies(lista_arboles))
+    arbol_e_inclinacion = dict()
+    especie_mas_inclinada = ""
+    for i in conjunto_especies_lista :
+        arbol_e_inclinacion[i] = max(obtener_inclinaciones(lista_arboles, i))
+    mayor_inclinacion = max(list(arbol_e_inclinacion.values()) )
+    for j in list(arbol_e_inclinacion.items()) :
+        if j[1] == mayor_inclinacion :
+            especie_mas_inclinada = j[0] 
+    print("Especie con mayor inclinacion:",especie_mas_inclinada,"con",mayor_inclinacion)
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ") 
+# b = leer_parque("arbolado-en-espacios-verdes.csv", "CENTENARIO")           
+# especimen_mas_inclinado(b)
+
+        
+# # Ejercici7--------------------------------------
+def especie_promedio_mas_inclinada(lista_arboles):
+    conjunto_especies_lista = list(especies(lista_arboles))
+    arbol_e_inclinacionProm = dict()
+    especieMayorPromedioInclinacion = ""
+    for i in conjunto_especies_lista :
+        suma_inclinaciones = sum(obtener_inclinaciones(lista_arboles, i))
+        cantidad_inclinaciones = len(obtener_inclinaciones(lista_arboles, i))
+        arbol_e_inclinacionProm[i] = suma_inclinaciones / cantidad_inclinaciones
+    mayorPromedioInclinacion = max(list(arbol_e_inclinacionProm.values()) )
+    for j in list(arbol_e_inclinacionProm.items()) :
+        if j[1] == mayorPromedioInclinacion :
+            especieMayorPromedioInclinacion = j[0]
+    print("Especie con mayor inclinacion promedio es:",especieMayorPromedioInclinacion,"con",mayorPromedioInclinacion)
+# a = leer_parque("arbolado-en-espacios-verdes.csv", "GENERAL PAZ") 
+# b = leer_parque("arbolado-en-espacios-verdes.csv", "ANDES, LOS")           
+# especie_promedio_mas_inclinada(b)
+
+   
+
+
+
+
+    
+
+        
+            
+            
+    
+
+    
+    
+    
