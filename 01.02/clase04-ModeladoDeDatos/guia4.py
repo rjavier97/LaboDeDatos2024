2611f693f65e45e8e6f573f1284006823b2d4934
rjavier97 <rjavier.suarez@hotmail.com>
2024-02-07 20:39:32 -0300

    guia4 DER y Python terminada

--- 01.02/clase04-ModeladoDeDatos/guia4.py
+++ 01.02/clase04-ModeladoDeDatos/guia4.py
@@ -24,10 +24,11 @@ def superanSalarioActividad01(empleado01, umbral)->list:
 empleado02 = empleado01.copy()
 empleado02.append([43967304, 37, 0, 12000])
 empleado02.append([42236276, 36, 0, 18000])
-
+empleado02
 # superanSalarioActividad01(empleado02, 15000)
 
 # Si, sigue funcionando 
+
 
 # Ejercicio3---------------------------------------------
 empleado03 = [[20222333, 20000, 45, 2],
@@ -42,20 +43,36 @@ empleado03 = [[20222333, 20000, 45, 2],
 
 def superanSalarioActividad03(empleado03, umbral)->list:
     empleado03original= []
-    lista = []
-    for i in empleado03 :
-        lista.append(i[0])
-        lista.append(i[2])
-        lista.append(i[3])
-        lista.append(i[1])
-        empleado03original.append(lista)
-    nuevaMatriz = []  
-    print(empleado03original)
+    matrizResultado = []
+    for fila in empleado03 :
+        empleado03original.append(fila[:1] + fila[2:4] + fila[1:2]) #reordeno la matriz  
     for fila in empleado03original :
         if fila[3] > umbral :     
-            nuevaMatriz.append(fila)
-    return nuevaMatriz 
-superanSalarioActividad03(empleado03, 15000)
+            matrizResultado.append(fila)
+    return matrizResultado
+# superanSalarioActividad03(empleado03, 15000)
+
+
+# Ejercicio4---------------------------------------------
+
+empleado04 = [[20222333, 33456234, 45432345, 43967304, 42236276],
+              [20000, 25000, 10000, 12000, 18000],
+              [45, 40, 41, 37, 36],
+              [2, 0, 1, 0, 0]]
+
+def superanSalarioActividad04(empleado04, umbral)->list:
+    columnasxfilas = []
+    matrizResultado = []
+    for columna in range(0,len(empleado04[0])) :
+        b=[ empleado04[0][columna], empleado04[2][columna], empleado04[3][columna], empleado04[1][columna] ] 
+        columnasxfilas.append(b)
+    for fila in columnasxfilas :
+        if fila[3] > 15000 :
+            matrizResultado.append(fila)
+    return matrizResultado 
+# superanSalarioActividad04(empleado04, 15000)
+
+
 
             
         
